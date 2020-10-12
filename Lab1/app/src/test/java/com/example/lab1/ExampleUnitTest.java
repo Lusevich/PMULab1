47fb8c8c11a8319e867c533873fc641018b84a08
Lusevich <72742078+Lusevich@users.noreply.github.com>
2020-10-12 19:38:40 +0700

    Add Lab1

---
+++ Lab1/app/src/test/java/com/example/lab1/ExampleUnitTest.java
@@ -0,0 +1,50 @@
+package com.example.lab1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development
+ * machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing
+ * documentation</a>
+ */
+public class UtilsTest {
+    @Test
+    public void max_correct_one_value() {
+        assertEquals(5, Utils.max(5));
+    }
+
+    @Test
+    public void max_correct_two_values() {
+        assertEquals(5, Utils.max(5, 0));
+    }
+
+    @Test
+    public void max_correct_three_values() { assertEquals(10, Utils.max(5, 10, 1)); }
+
+    @Test
+    public void max_correct_many_values() {
+        assertEquals(100, Utils.max(2, 66, 22, 1, 66, 100, 100, 99));
+    }
+
+    @Test
+    public void min_correct_one_value() {
+        assertEquals(5, Utils.max(5));
+    }
+
+    @Test
+    public void min_correct_two_values() {
+        assertEquals(0, Utils.max(5, 0));
+    }
+
+    @Test
+    public void min_correct_three_values() { assertEquals(1, Utils.max(5, 10, 1)); }
+
+    @Test
+    public void min_correct_many_values() {
+        assertEquals(1, Utils.max(2, 66, 22, 1, 66, 100, 100, 99));
+    }
+}
